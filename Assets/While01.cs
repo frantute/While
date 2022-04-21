67376c9da0f9b30fe53bc532892da3f3a5c8c48e
frantute <102159238+frantute@users.noreply.github.com>
2022-04-21 08:59:35 -0300

    while01 terminado

---
+++ Assets/While01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class While01 : MonoBehaviour
+{
+    public int counter;
+    public int countermax;
+    public Text txt_display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter<=countermax)
+        {
+            Debug.Log("Repeticion");
+            counter++;
+        }
+        txt_display.text = "Termino el while";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
